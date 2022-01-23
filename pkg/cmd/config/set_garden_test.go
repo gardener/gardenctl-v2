f270050b74c2af8985806de66c5bdeb9176a7d9d
Holger Koser <holger.koser@sap.com>
2022-01-23 18:00:56 +0100

    added subcommand set-garden units for 100% coverage

--- pkg/cmd/config/set_garden_test.go
+++ pkg/cmd/config/set_garden_test.go
@@ -11,8 +11,11 @@ import (
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/types"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 
 	cmdconfig "github.com/gardener/gardenctl-v2/pkg/cmd/config"
+	"github.com/gardener/gardenctl-v2/pkg/config"
 )
 
 var _ = Describe("Config Command - SetGarden", func() {
@@ -61,3 +64,156 @@ var _ = Describe("Config Command - SetGarden Options", func() {
 		Entry("when garden empty", "", Not(Succeed())),
 	)
 })
+
+var _ = Describe("Config Subcommand SetGarden", func() {
+	Describe("Instance", func() {
+		var cmd *cobra.Command
+
+		BeforeEach(func() {
+			cmd = cmdconfig.NewCmdConfigSetGarden(factory, streams)
+		})
+
+		It("should have Use, ValidArgsFunction and Flags", func() {
+			Expect(cmd.Use).To(Equal("set-garden"))
+			Expect(cmd.ValidArgsFunction).NotTo(BeNil())
+			Expect(cmd.ValidArgs).To(BeNil())
+			assertAllFlagNames(cmd.Flags(), "context", "kubeconfig", "pattern")
+		})
+	})
+
+	Describe("Options", func() {
+		var options *cmdconfig.SetGardenOptions
+
+		BeforeEach(func() {
+			options = cmdconfig.NewSetGardenOptions()
+			options.IOStreams = streams
+		})
+
+		Describe("Complete", func() {
+			Context("when getting configuration fails", func() {
+				It("should fail", func() {
+					factory.EXPECT().Manager().Return(manager, nil)
+					manager.EXPECT().Configuration().Return(nil)
+					Expect(options.Complete(factory, nil, nil)).To(MatchError("failed to get configuration"))
+				})
+			})
+
+			Context("when getting configuration succeeds", func() {
+				It("should succeed", func() {
+					factory.EXPECT().Manager().Return(manager, nil)
+					manager.EXPECT().Configuration().Return(cfg)
+					Expect(options.Complete(factory, nil, []string{" garden "})).To(Succeed())
+					Expect(options.Configuration).To(BeIdenticalTo(cfg))
+					Expect(options.Name).To(Equal("garden"))
+				})
+			})
+		})
+
+		Describe("Validate", func() {
+			DescribeTable("Validating Name Argument",
+				func(name string, matcher types.GomegaMatcher) {
+					o := cmdconfig.NewSetGardenOptions()
+					o.Name = name
+					Expect(o.Validate()).To(matcher)
+				},
+				Entry("when garden is foo", "foo", Succeed()),
+				Entry("when garden empty", "", MatchError("garden identity is required")),
+			)
+		})
+
+		Describe("Run", func() {
+			const pathToKubeconfig = "/path/to/kubeconfig"
+			const testContext = "test-context"
+			const testPattern = "test-pattern"
+
+			BeforeEach(func() {
+				options.Configuration = cfg
+			})
+
+			It("should add new garden to configuration", func() {
+				options.Name = gardenIdentity3
+				Expect(options.KubeconfigFlag.Set(pathToKubeconfig)).To(Succeed())
+				Expect(options.Run(nil)).To(Succeed())
+
+				assertGardenNames(cfg, gardenIdentity1, gardenIdentity2, gardenIdentity3)
+				assertGarden(cfg, &config.Garden{
+					Name:       gardenIdentity3,
+					Kubeconfig: pathToKubeconfig,
+				})
+				assertConfigHasBeenSaved(cfg)
+				Expect(out.String()).To(MatchRegexp("^Successfully configured garden"))
+			})
+
+			It("should modify an existing garden configuration", func() {
+				options.Name = gardenIdentity1
+				Expect(options.ContextFlag.Set(testContext)).To(Succeed())
+				options.Patterns = []string{testPattern}
+				Expect(options.Run(nil)).To(Succeed())
+
+				assertGardenNames(cfg, gardenIdentity1, gardenIdentity2)
+				assertGarden(cfg, &config.Garden{
+					Name:       gardenIdentity1,
+					Kubeconfig: kubeconfig,
+					Context:    testContext,
+					Patterns:   []string{testPattern},
+				})
+				assertConfigHasBeenSaved(cfg)
+				Expect(out.String()).To(MatchRegexp("^Successfully configured garden"))
+			})
+
+			It("should remove all patterns from an existing configuration", func() {
+				options.Name = gardenIdentity2
+				Expect(options.KubeconfigFlag.Set(pathToKubeconfig)).To(Succeed())
+				options.Patterns = []string{""}
+				Expect(options.Run(nil)).To(Succeed())
+
+				assertGardenNames(cfg, gardenIdentity1, gardenIdentity2)
+				assertGarden(cfg, &config.Garden{
+					Name:       gardenIdentity2,
+					Kubeconfig: pathToKubeconfig,
+					Patterns:   []string{},
+				})
+				assertConfigHasBeenSaved(cfg)
+				Expect(out.String()).To(MatchRegexp("^Successfully configured garden"))
+			})
+
+			It("should fail when the filename is invalid", func() {
+				options.Configuration.Filename = string([]byte{0})
+				Expect(options.Run(nil)).To(MatchError(MatchRegexp("^failed to configure garden")))
+			})
+		})
+	})
+})
+
+func assertAllFlagNames(flags *pflag.FlagSet, expNames ...string) {
+	actNames := []string{}
+
+	flags.VisitAll(func(flag *pflag.Flag) {
+		actNames = append(actNames, flag.Name)
+	})
+
+	ExpectWithOffset(1, actNames).To(Equal(expNames))
+}
+
+func assertGardenNames(cfg *config.Config, names ...string) {
+	ExpectWithOffset(1, cfg.GardenNames()).To(Equal(names))
+}
+
+func assertGarden(cfg *config.Config, garden *config.Garden) {
+	g, err := cfg.Garden(garden.Name)
+	ExpectWithOffset(1, err).ToNot(HaveOccurred())
+	ExpectWithOffset(1, g).To(BeEquivalentTo(garden))
+}
+
+func assertConfigHasBeenSaved(cfg *config.Config) {
+	c, err := config.LoadFromFile(cfg.Filename)
+	ExpectWithOffset(1, err).NotTo(HaveOccurred())
+
+	for i, g := range cfg.Gardens {
+		if len(g.Patterns) == 0 {
+			cfg.Gardens[i].Patterns = nil
+		}
+	}
+
+	ExpectWithOffset(1, c).To(BeEquivalentTo(cfg))
+}
