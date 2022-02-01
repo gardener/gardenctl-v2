ab91fca282f2202b7391cd07edb3f1c4f6ed27d4
Peter Sutter <peter.sutter@sap.com>
2022-02-01 12:30:19 +0100

    Generate Markdown from commands (#87)
    * Add markdown generator for gardenctl command
    
    * go mod tidy
    
    * Adapt help format for better readability in .md
    
    * go run ./internal/gen/markdown.go

---
+++ internal/gen/markdown.go
@@ -0,0 +1,24 @@
+/*
+SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Gardener contributors
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"log"
+
+	"github.com/spf13/cobra/doc"
+
+	"github.com/gardener/gardenctl-v2/pkg/cmd"
+)
+
+func main() {
+	gardenctl := cmd.NewDefaultGardenctlCommand()
+	err := doc.GenMarkdownTree(gardenctl, "./docs")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
