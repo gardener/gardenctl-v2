a6b1fda9c5c653c8355be4b0323f568f81fecee9
Lukas Gross <lukas.gross@sap.com>
2022-02-07 13:18:11 +0100

    Removed needless target flag override logic

--- internal/util/target.go
+++ internal/util/target.go
@@ -11,8 +11,9 @@ import (
 	"errors"
 	"fmt"
 
-	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	"k8s.io/apimachinery/pkg/util/sets"
+
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 
 	"github.com/gardener/gardenctl-v2/internal/gardenclient"
 	"github.com/gardener/gardenctl-v2/pkg/target"
@@ -24,7 +25,12 @@ func ShootForTarget(ctx context.Context, gardenClient gardenclient.Client, t tar
 }
 
 // ShootNamesForTarget returns all possible shoots for a given target.
-func ShootNamesForTarget(ctx context.Context, manager target.Manager, t target.Target) ([]string, error) {
+func ShootNamesForTarget(ctx context.Context, manager target.Manager) ([]string, error) {
+	t, err := manager.CurrentTarget()
+	if err != nil {
+		return nil, err
+	}
+
 	gardenClient, err := manager.GardenClient(t.GardenName())
 	if err != nil {
 		return nil, fmt.Errorf("failed to create Kubernetes client for garden cluster %q: %w", t.GardenName(), err)
@@ -55,7 +61,12 @@ func SeedForTarget(ctx context.Context, gardenClient gardenclient.Client, t targ
 
 // SeedNamesForTarget returns all possible seeds for a given target. The
 // target must at least point to a garden.
-func SeedNamesForTarget(ctx context.Context, manager target.Manager, t target.Target) ([]string, error) {
+func SeedNamesForTarget(ctx context.Context, manager target.Manager) ([]string, error) {
+	t, err := manager.CurrentTarget()
+	if err != nil {
+		return nil, err
+	}
+
 	gardenClient, err := manager.GardenClient(t.GardenName())
 	if err != nil {
 		return nil, fmt.Errorf("failed to create Kubernetes client for garden cluster %q: %w", t.GardenName(), err)
@@ -86,7 +97,12 @@ func ProjectForTarget(ctx context.Context, gardenClient gardenclient.Client, t t
 
 // ProjectNamesForTarget returns all projects for the targeted garden.
 // target must at least point to a garden.
-func ProjectNamesForTarget(ctx context.Context, manager target.Manager, t target.Target) ([]string, error) {
+func ProjectNamesForTarget(ctx context.Context, manager target.Manager) ([]string, error) {
+	t, err := manager.CurrentTarget()
+	if err != nil {
+		return nil, err
+	}
+	
 	gardenClient, err := manager.GardenClient(t.GardenName())
 	if err != nil {
 		return nil, fmt.Errorf("failed to create Kubernetes client for garden cluster %q: %w", t.GardenName(), err)
--- pkg/cmd/cmd.go
+++ pkg/cmd/cmd.go
@@ -208,31 +208,18 @@ func registerCompletionFuncForGlobalFlags(cmd *cobra.Command, f *util.FactoryImp
 }
 
 type cobraCompletionFunc func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective)
-type cobraCompletionFuncWithError func(ctx context.Context, manager target.Manager, tf target.TargetFlags) ([]string, error)
+type cobraCompletionFuncWithError func(ctx context.Context, manager target.Manager) ([]string, error)
 
 func completionWrapper(f *util.FactoryImpl, ioStreams util.IOStreams, completer cobraCompletionFuncWithError) cobraCompletionFunc {
 	return func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-		tf := f.TargetFlags
-
-		// By default, the factory will provide a target manager that uses the dynamicTargetProvider (DTP)
-		// implementation, i.e. is based on the file just as much as the CLI flags.
-		// The DTP tries to allow users to "move up", i.e. when they already targeted a shoot, just adding
-		// "--garden foo" should not just change the used garden cluster, but _target_ the garden (instead
-		// of the shoot). This behaviour is not suitable for the CLI completion functions, because
-		// when completing "gardenctl --garden foo --shoot [tab]", the DTP would consider this as
-		// "user wants to target the garden" and will therefore throw away the project/seed information.
-		// Project and seed information however are important for the completion functions.
-		//
-		// To work around this, all completion functions use a manager with a regular filesystem based
-		// target provider without considering the given target flags.
-		manager, err := f.WithoutTargetFlags().Manager()
+		manager, err := f.Manager()
 
 		if err != nil {
 			fmt.Fprintf(ioStreams.ErrOut, "%v\n", err)
 			return nil, cobra.ShellCompDirectiveNoFileComp
 		}
 
-		result, err := completer(f.Context(), manager, tf)
+		result, err := completer(f.Context(), manager)
 		if err != nil {
 			fmt.Fprintf(ioStreams.ErrOut, "%v\n", err)
 			return nil, cobra.ShellCompDirectiveNoFileComp
@@ -242,55 +229,20 @@ func completionWrapper(f *util.FactoryImpl, ioStreams util.IOStreams, completer 
 	}
 }
 
-func getCurrentTarget(tf target.TargetFlags, manager target.Manager) (target.Target, error) {
-	// any --garden flag has precedence over the config file
-	if tf.GardenName() != "" {
-		return target.NewTarget(tf.GardenName(), "", "", ""), nil
-	}
-
-	currentTarget, err := manager.CurrentTarget()
-	if err != nil {
-		return nil, fmt.Errorf("failed to read current target: %w", err)
-	}
-
-	if tf.ProjectName() != "" {
-		currentTarget = currentTarget.WithProjectName(tf.ProjectName()).WithSeedName("")
-	} else if tf.SeedName() != "" {
-		currentTarget = currentTarget.WithSeedName(tf.SeedName()).WithProjectName("")
-	}
-
-	return currentTarget, nil
-}
-
-func gardenFlagCompletionFunc(ctx context.Context, manager target.Manager, tf target.TargetFlags) ([]string, error) {
+func gardenFlagCompletionFunc(ctx context.Context, manager target.Manager) ([]string, error) {
 	return util.GardenNames(manager)
 }
 
-func projectFlagCompletionFunc(ctx context.Context, manager target.Manager, tf target.TargetFlags) ([]string, error) {
-	currentTarget, err := getCurrentTarget(tf, manager)
-	if err != nil {
-		return nil, err
-	}
-
-	return util.ProjectNamesForTarget(ctx, manager, currentTarget)
+func projectFlagCompletionFunc(ctx context.Context, manager target.Manager) ([]string, error) {
+	return util.ProjectNamesForTarget(ctx, manager)
 }
 
-func seedFlagCompletionFunc(ctx context.Context, manager target.Manager, tf target.TargetFlags) ([]string, error) {
-	currentTarget, err := getCurrentTarget(tf, manager)
-	if err != nil {
-		return nil, err
-	}
-
-	return util.SeedNamesForTarget(ctx, manager, currentTarget)
+func seedFlagCompletionFunc(ctx context.Context, manager target.Manager) ([]string, error) {
+	return util.SeedNamesForTarget(ctx, manager)
 }
 
-func shootFlagCompletionFunc(ctx context.Context, manager target.Manager, tf target.TargetFlags) ([]string, error) {
-	currentTarget, err := getCurrentTarget(tf, manager)
-	if err != nil {
-		return nil, err
-	}
-
-	return util.ShootNamesForTarget(ctx, manager, currentTarget)
+func shootFlagCompletionFunc(ctx context.Context, manager target.Manager) ([]string, error) {
+	return util.ShootNamesForTarget(ctx, manager)
 }
 
 // addKlogFlags adds flags from k8s.io/klog
