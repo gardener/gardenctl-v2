0259ab87ca636bc6db91ec2674710934225d00fc
Peter Sutter <peter.sutter@sap.com>
2022-02-10 13:55:26 +0100

    Bump dependencies (#98)
    * Bump go mod dependencies
    
    * bump golangci-lint version
    
    * go mod edit -go=1.17
    
    * use golang:1.17

--- hack/golangci-lint.sh
+++ hack/golangci-lint.sh
@@ -17,7 +17,7 @@ else
 fi
 
 # Install golangci-lint (linting tool)
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.43.0
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.44.0
 
 cd "$SOURCE_PATH"
 
