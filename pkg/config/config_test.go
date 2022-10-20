58d428e00f4afabd19184ccdf98180c1354a1dad
Sven Petersen <5971734+sven-petersen@users.noreply.github.com>
2022-10-20 14:54:44 +0200

    bugfix - config file creation failed when directory did not exist

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -162,4 +162,16 @@ var _ = Describe("Config", func() {
 		Entry("when LinkKubeconfig is false and envVar is True", pointer.Bool(false), "True", pointer.Bool(true)),
 		Entry("when LinkKubeconfig is false and envVar is False", pointer.Bool(false), "False", pointer.Bool(false)),
 	)
+
+	It("should create configuration directories and file", func() {
+		dir, err := os.MkdirTemp("", "garden-*")
+		Expect(err).NotTo(HaveOccurred())
+		defer os.RemoveAll(dir)
+
+		filename := filepath.Join(dir, "./.nonexisting-sub-folder/", "gardenctl-v2.yaml")
+		cfg = &config.Config{
+			Filename: filename,
+		}
+		Expect(cfg.Save()).NotTo(HaveOccurred())
+	})
 })
