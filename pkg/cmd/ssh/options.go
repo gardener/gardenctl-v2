17c27d2323f090eaf705ef9f6a73cec3a3f6f146
Ted Teng <ted.teng@sap.com>
2022-12-09 10:35:47 +0800

    error message log line change

--- pkg/cmd/ssh/options.go
+++ pkg/cmd/ssh/options.go
@@ -768,7 +768,7 @@ func waitForBastion(ctx context.Context, o *SSHOptions, gardenClient client.Clie
 
 		lastCheckErr = bastionAvailabilityChecker(preferredBastionAddress(bastion), privateKeyBytes)
 		if lastCheckErr != nil {
-			fmt.Fprintf(o.IOStreams.ErrOut, "Still waiting: Bastion is ready, waiting to establish an ssh connection with bastion: %v\n", lastCheckErr)
+			fmt.Fprintf(o.IOStreams.ErrOut, "Still waiting: Bastion is ready, however cannot connect to bastion yet: %v\n", lastCheckErr)
 			return false, nil
 		}
 
