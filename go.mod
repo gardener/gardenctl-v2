2ff410caa7a263bd912454de417dc76dbe823ddd
Peter Sutter <peter.sutter@sap.com>
2023-03-20 17:31:31 +0100

    use sigs.k8s.io yaml encoder

--- go.mod
+++ go.mod
@@ -27,6 +27,7 @@ require (
 	k8s.io/klog/v2 v2.70.1
 	k8s.io/utils v0.0.0-20220728103510-ee6ede2d64ed
 	sigs.k8s.io/controller-runtime v0.13.0
+	sigs.k8s.io/yaml v1.3.0
 )
 
 require (
@@ -131,7 +132,6 @@ require (
 	sigs.k8s.io/kustomize/api v0.12.1 // indirect
 	sigs.k8s.io/kustomize/kyaml v0.13.9 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
-	sigs.k8s.io/yaml v1.3.0 // indirect
 )
 
 replace k8s.io/client-go => k8s.io/client-go v0.25.0
--- pkg/cmd/base/options.go
+++ pkg/cmd/base/options.go
@@ -13,7 +13,7 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
-	"gopkg.in/yaml.v3"
+	"sigs.k8s.io/yaml"
 
 	"github.com/gardener/gardenctl-v2/internal/util"
 )
@@ -75,15 +75,12 @@ func (o *Options) PrintObject(obj interface{}) error {
 		fmt.Fprintf(o.IOStreams.Out, "%v", obj)
 
 	case "yaml":
-		yamlEncoder := yaml.NewEncoder(o.IOStreams.Out)
-		defer yamlEncoder.Close()
-
-		yamlEncoder.SetIndent(2)
-
-		err := yamlEncoder.Encode(&obj)
+		marshalled, err := yaml.Marshal(&obj)
 		if err != nil {
 			return err
 		}
+
+		fmt.Fprintln(o.IOStreams.Out, string(marshalled))
 	case "json":
 		marshalled, err := json.MarshalIndent(&obj, "", "  ")
 		if err != nil {
