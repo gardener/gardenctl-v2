3ce9ad5fa39fe086c77f384c48820e2c7541eaee
Peter Sutter <peter.sutter@sap.com>
2023-03-27 21:38:23 +0200

    Add generate-sequential make target

---
+++ hack/tools.go
@@ -0,0 +1,15 @@
+//go:build tools
+// +build tools
+
+/*
+SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and Gardener contributors
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+// This package imports things required by build scripts, to force `go mod` to see them as dependencies
+package tools
+
+import (
+	_ "github.com/golang/mock/mockgen"
+)
