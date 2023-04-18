1dc598b1493a19a42100dc8dab3a7492c38cac86
Holger Koser <holger.koser@sap.com>
2023-04-18 17:03:57 +0200

    Add OS_APPLICATION_CREDENTIAL_xxx environment variables to template

--- pkg/cmd/env/testdata/openstack/export.bash
+++ pkg/cmd/env/testdata/openstack/export.bash
@@ -1,13 +1,11 @@
-export OS_IDENTITY_API_VERSION='3';
-export OS_AUTH_VERSION='3';
-export OS_AUTH_STRATEGY='keystone';
 export OS_AUTH_URL='keyStoneURL';
-export OS_TENANT_NAME='tenant';
 export OS_PROJECT_DOMAIN_NAME='domain';
 export OS_USER_DOMAIN_NAME='domain';
+export OS_REGION_NAME='europe';
+export OS_AUTH_STRATEGY='keystone';
+export OS_TENANT_NAME='tenant';
 export OS_USERNAME='user';
 export OS_PASSWORD='secret';
-export OS_REGION_NAME='europe';
 
 # Run this command to configure openstack for your shell:
 # eval $(gardenctl provider-env bash)
