238c1e00d5126695eb1f3d7904efb4faeb3a20e0
Holger Koser <holger.koser@sap.com>
2023-04-19 11:33:51 +0200

    always set all env vars

--- pkg/cmd/env/testdata/openstack/export.bash
+++ pkg/cmd/env/testdata/openstack/export.bash
@@ -6,6 +6,10 @@ export OS_AUTH_STRATEGY='keystone';
 export OS_TENANT_NAME='tenant';
 export OS_USERNAME='user';
 export OS_PASSWORD='secret';
+export OS_AUTH_TYPE='';
+export OS_APPLICATION_CREDENTIAL_ID='';
+export OS_APPLICATION_CREDENTIAL_NAME='';
+export OS_APPLICATION_CREDENTIAL_SECRET='';
 
 # Run this command to configure openstack for your shell:
 # eval $(gardenctl provider-env bash)
