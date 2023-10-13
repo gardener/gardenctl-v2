6d1fdc31f028d576fe0a985ca4fe8da24b82edca
Dimitar Mirchev <dimitar.mirchev@sap.com>
2023-10-13 10:37:14 +0300

    Increase rsa key size to 4096

--- pkg/cmd/ssh/options.go
+++ pkg/cmd/ssh/options.go
@@ -427,7 +427,7 @@ func createSSHKeypair(tempDir string, keyName string) (PrivateKeyFile, PublicKey
 
 func createSSHPrivateKey() (*rsa.PrivateKey, error) {
 	// Private Key generation
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
 	if err != nil {
 		return nil, err
 	}
