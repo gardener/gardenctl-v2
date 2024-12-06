5e6f44ae9714aa757f6f1c1ecb36f3b1f71b0b82
Marc Vornetran <marc.vornetran@sap.com>
2024-12-06 16:21:52 +0100

    docs: Generate extended long text for kubectl-env sub-commands

--- pkg/cmd/kubectlenv/kubectlenv.go
+++ pkg/cmd/kubectlenv/kubectlenv.go
@@ -32,18 +32,30 @@ func NewCmdKubectlEnv(f util.Factory, ioStreams util.IOStreams) *cobra.Command {
 See each sub-command's help for details on how to use the generated script.
 
 The generated script points the KUBECONFIG environment variable to the currently targeted shoot, seed or garden cluster.
+To point the KUBECONFIG to the targeted cluster in all shell sessions consider adding the script to your shell's startup configuration.
 `,
 		Aliases: []string{"k-env", "cluster-env"},
 	}
 	o.AddFlags(cmd.PersistentFlags())
 
 	for _, s := range env.ValidShells() {
+		prompt := s.Prompt(runtime.GOOS)
+		evalCommand := s.EvalCommand(fmt.Sprintf("gardenctl %s %s", cmd.Name(), s))
 		cmd.AddCommand(&cobra.Command{
 			Use:   string(s),
 			Short: fmt.Sprintf("Generate a script that points KUBECONFIG to the targeted cluster for %s", s),
-			Long: fmt.Sprintf("Generate a script that points KUBECONFIG to the targeted cluster for %s.\n\n"+
-				"To load the kubectl configuration script in your current shell session:\n%s\n",
-				s, s.Prompt(runtime.GOOS)+s.EvalCommand(fmt.Sprintf("gardenctl %s %s", cmd.Name(), s)),
+			Long: fmt.Sprintf(`Generate a script that points KUBECONFIG to the targeted cluster for %s.
+
+To load the kubectl configuration script in your current shell session:
+%s
+
+To load the kubectl configuration for each shell session add the following line at the end of the %s file:
+
+    %s
+
+You will need to start a new shell for this setup to take effect.
+`,
+				s, prompt+evalCommand, s.Config(), evalCommand,
 			),
 			RunE: runE,
 		})
