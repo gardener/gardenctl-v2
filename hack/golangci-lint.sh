68add85c0747aab3fc74727de5cc72ab8091ccba
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2025-03-03 01:33:14 +0100

    Update dependency golangci/golangci-lint to v1.64.6 (#523)

--- hack/golangci-lint.sh
+++ hack/golangci-lint.sh
@@ -17,7 +17,7 @@ else
 fi
 
 # renovate: datasource=github-releases depName=golangci/golangci-lint
-golangci_lint_version=v1.64.5
+golangci_lint_version=v1.64.6
 
 GOLANGCI_LINT_ADDITIONAL_FLAGS=${GOLANGCI_LINT_ADDITIONAL_FLAGS:-""}
 
