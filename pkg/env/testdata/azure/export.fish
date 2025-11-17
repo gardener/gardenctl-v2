56cb3163eb32109808cd283dc71be3173d646707
Peter Sutter <peter.sutter@sap.com>
2025-11-17 14:32:25 +0100

    refactor: prefix credential temp files with dot for hidden file convention

--- pkg/env/testdata/azure/export.fish
+++ pkg/env/testdata/azure/export.fish
@@ -1,16 +1,16 @@
-set -gx AZURE_CLIENT_ID (cat -- 'PLACEHOLDER_SESSION_DIR/provider-env/PLACEHOLDER_HASH-clientID.txt');
-set -gx AZURE_TENANT_ID (cat -- 'PLACEHOLDER_SESSION_DIR/provider-env/PLACEHOLDER_HASH-tenantID.txt');
-set -gx AZURE_SUBSCRIPTION_ID (cat -- 'PLACEHOLDER_SESSION_DIR/provider-env/PLACEHOLDER_HASH-subscriptionID.txt');
+set -gx AZURE_CLIENT_ID (cat -- 'PLACEHOLDER_SESSION_DIR/provider-env/.PLACEHOLDER_HASH-clientID.txt');
+set -gx AZURE_TENANT_ID (cat -- 'PLACEHOLDER_SESSION_DIR/provider-env/.PLACEHOLDER_HASH-tenantID.txt');
+set -gx AZURE_SUBSCRIPTION_ID (cat -- 'PLACEHOLDER_SESSION_DIR/provider-env/.PLACEHOLDER_HASH-subscriptionID.txt');
 set -gx AZURE_CONFIG_DIR 'PLACEHOLDER_CONFIG_DIR';
-set AZURE_CLIENT_SECRET (cat -- 'PLACEHOLDER_SESSION_DIR/provider-env/PLACEHOLDER_HASH-clientSecret.txt');
+set AZURE_CLIENT_SECRET (cat -- 'PLACEHOLDER_SESSION_DIR/provider-env/.PLACEHOLDER_HASH-clientSecret.txt');
 az login --service-principal --username "$AZURE_CLIENT_ID" --password "$AZURE_CLIENT_SECRET" --tenant "$AZURE_TENANT_ID";
 set -e AZURE_CLIENT_SECRET;
 az account set --subscription "$AZURE_SUBSCRIPTION_ID";
-rm -f -- 'PLACEHOLDER_SESSION_DIR/provider-env/PLACEHOLDER_HASH-clientID.txt';
-rm -f -- 'PLACEHOLDER_SESSION_DIR/provider-env/PLACEHOLDER_HASH-clientSecret.txt';
-rm -f -- 'PLACEHOLDER_SESSION_DIR/provider-env/PLACEHOLDER_HASH-region.txt';
-rm -f -- 'PLACEHOLDER_SESSION_DIR/provider-env/PLACEHOLDER_HASH-subscriptionID.txt';
-rm -f -- 'PLACEHOLDER_SESSION_DIR/provider-env/PLACEHOLDER_HASH-tenantID.txt';
+rm -f -- 'PLACEHOLDER_SESSION_DIR/provider-env/.PLACEHOLDER_HASH-clientID.txt';
+rm -f -- 'PLACEHOLDER_SESSION_DIR/provider-env/.PLACEHOLDER_HASH-clientSecret.txt';
+rm -f -- 'PLACEHOLDER_SESSION_DIR/provider-env/.PLACEHOLDER_HASH-region.txt';
+rm -f -- 'PLACEHOLDER_SESSION_DIR/provider-env/.PLACEHOLDER_HASH-subscriptionID.txt';
+rm -f -- 'PLACEHOLDER_SESSION_DIR/provider-env/.PLACEHOLDER_HASH-tenantID.txt';
 printf 'Run the following command to log out and remove access to Azure subscriptions:\n$ eval (gardenctl provider-env --garden test --project project --shoot shoot -u fish)\n';
 
 # Run this command to configure az for your shell:
--- pkg/env/testdata/gcp/export.seed.bash
+++ pkg/env/testdata/gcp/export.seed.bash
@@ -1,11 +1,11 @@
-export CLOUDSDK_CORE_PROJECT=$(< 'PLACEHOLDER_SESSION_DIR/provider-env/PLACEHOLDER_HASH-project_id.txt');
-export CLOUDSDK_COMPUTE_REGION=$(< 'PLACEHOLDER_SESSION_DIR/provider-env/PLACEHOLDER_HASH-region.txt');
+export CLOUDSDK_CORE_PROJECT=$(< 'PLACEHOLDER_SESSION_DIR/provider-env/.PLACEHOLDER_HASH-project_id.txt');
+export CLOUDSDK_COMPUTE_REGION=$(< 'PLACEHOLDER_SESSION_DIR/provider-env/.PLACEHOLDER_HASH-region.txt');
 export CLOUDSDK_CONFIG='PLACEHOLDER_CONFIG_DIR';
-gcloud auth login --cred-file 'PLACEHOLDER_SESSION_DIR/provider-env/PLACEHOLDER_HASH-credentials.txt';
+gcloud auth login --cred-file 'PLACEHOLDER_SESSION_DIR/provider-env/.PLACEHOLDER_HASH-credentials.txt';
 export GOOGLE_CREDENTIALS_ACCOUNT=$(gcloud auth list --filter=status:ACTIVE --format='value(account)');
-rm -f -- 'PLACEHOLDER_SESSION_DIR/provider-env/PLACEHOLDER_HASH-credentials.txt';
-rm -f -- 'PLACEHOLDER_SESSION_DIR/provider-env/PLACEHOLDER_HASH-project_id.txt';
-rm -f -- 'PLACEHOLDER_SESSION_DIR/provider-env/PLACEHOLDER_HASH-region.txt';
+rm -f -- 'PLACEHOLDER_SESSION_DIR/provider-env/.PLACEHOLDER_HASH-credentials.txt';
+rm -f -- 'PLACEHOLDER_SESSION_DIR/provider-env/.PLACEHOLDER_HASH-project_id.txt';
+rm -f -- 'PLACEHOLDER_SESSION_DIR/provider-env/.PLACEHOLDER_HASH-region.txt';
 printf 'Run the following command to revoke access credentials:\n$ eval $(gardenctl provider-env --garden test --seed seed --shoot shoot -u bash)\n';
 
 # Run this command to configure gcloud for your shell:
