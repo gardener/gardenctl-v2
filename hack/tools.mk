347f1768baf6d3c79b60c5bbb8035aee850678b3
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2026-07-31 13:53:02 +0200

    Update dependency securego/gosec to v2.28.0 (#783)

--- hack/tools.mk
+++ hack/tools.mk
@@ -12,7 +12,7 @@ MOCKGEN                    := $(TOOLS_BIN_DIR)/mockgen
 
 # default tool versions
 # renovate: datasource=github-releases depName=securego/gosec
-GOSEC_VERSION ?= v2.27.1
+GOSEC_VERSION ?= v2.28.0
 
 export TOOLS_BIN_DIR := $(TOOLS_BIN_DIR)
 export PATH := $(abspath $(TOOLS_BIN_DIR)):$(PATH)
