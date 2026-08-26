9fc831acb426461d1dcf61caa52a85f873b9f837
Marc Vornetran <marc.vornetran@sap.com>
2026-08-26 15:15:45 +0200

    Update copyright text
    Signed-off-by: Marc Vornetran <marc.vornetran@sap.com>

--- hack/tools.go
+++ hack/tools.go
@@ -2,7 +2,7 @@
 // +build tools
 
 /*
-SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and Gardener contributors
+SPDX-FileCopyrightText: Copyright Contributors to the Gardener project
 
 SPDX-License-Identifier: Apache-2.0
 */
