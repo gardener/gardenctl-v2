f34336572bf4bcac2638fea441be5ec89ff27077
Neo Liang <neo.liang@sap.com>
2026-08-31 15:52:10 +0800

    add logout command

---
+++ pkg/cmd/logout/export_test.go
@@ -0,0 +1,19 @@
+/*
+SPDX-FileCopyrightText: Contributors to the Gardener project
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package logout
+
+// ParseExecArgs exposes parseExecArgs for testing.
+var ParseExecArgs = parseExecArgs
+
+// ReadIssuerFromCacheFile exposes readIssuerFromCacheFile for testing.
+var ReadIssuerFromCacheFile = readIssuerFromCacheFile
+
+// IssuerFromJWT exposes issuerFromJWT for testing.
+var IssuerFromJWT = issuerFromJWT
+
+// RemoveCachedTokens exposes removeCachedTokens for testing.
+var RemoveCachedTokens = removeCachedTokens
